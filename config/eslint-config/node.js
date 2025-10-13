c4a02dbc3b4522c768b42d85354df22c40de9112
pedro henrique <pedro.henrike2812@gmail.com>
2025-10-13 09:36:00 -0300

    chore: prettier and eslint config

---
+++ config/eslint-config/node.js
@@ -0,0 +1,58 @@
+import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import turboPlugin from 'eslint-plugin-turbo'
+import tseslint from 'typescript-eslint'
+import onlyWarn from 'eslint-plugin-only-warn'
+import rocketseatEslint from '@rocketseat/eslint-config'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+
+/**
+ * A shared ESLint configuration for the repository.
+ *
+ * @type {import("eslint").Linter.Config[]}
+ * */
+
+import { FlatCompat } from '@eslint/eslintrc'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+})
+
+const rocketseatReact = compat.extends('@rocketseat/eslint-config/node')
+
+export const config = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  ...rocketseatReact,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  {
+    plugins: {
+      turbo: turboPlugin,
+    },
+    rules: {
+      'turbo/no-undeclared-env-vars': 'warn',
+    },
+  },
+  {
+    plugins: {
+      onlyWarn,
+    },
+  },
+  {
+    ignores: ['dist/**'],
+  },
+]

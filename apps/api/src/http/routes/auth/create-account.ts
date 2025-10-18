cdcab0ed9411121066bdf19de1728fdddd9ebc09
pedro henrique <pedro.henrike2812@gmail.com>
2025-10-18 18:37:30 -0300

    feat: association email

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -7,6 +7,8 @@ import { hash } from "bcryptjs"
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post("/users", {
     schema: {
+      tags: ["Auth"],
+      summary: "Create a new account",
       body: z.object({
         name: z.string(),
         email: z.email(),
@@ -23,10 +25,19 @@ export async function createAccount(app: FastifyInstance) {
     })
 
     if (userWithSameEmail) {
-      return reply.status(409).send({
+      return reply.status(400).send({
         message: "User with same e-mail already exists."
       })
     }
+
+    const [, domain] = email.split("@")
+
+    const autoJoinOrganization = await prisma.organization.findFirst({
+      where: {
+        domain,
+        shouldAttachUsersByDomain: true,
+      }
+    })
 
     const passwordHash = await hash(password, 6)
 
@@ -35,6 +46,11 @@ export async function createAccount(app: FastifyInstance) {
         name,
         email,
         passwordHash,
+        member_on: autoJoinOrganization ? {
+          create: {
+            organizationId: autoJoinOrganization.id,
+          }
+        } : undefined
       }
     })
 

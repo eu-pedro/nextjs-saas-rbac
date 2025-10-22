c58446b27e24220b87e62b8ee2508b2edaca254c
pedro henrique <pedro.henrike2812@gmail.com>
2025-10-22 15:47:12 -0300

    feat: create a project

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -24,6 +24,7 @@ import { getOrganizations } from "./routes/orgs/get-organizations";
 import { updateOrganization } from "./routes/orgs/update-organization";
 import { shutdonwOrganization } from "./routes/orgs/shutdown-organization";
 import { transferOrganization } from "./routes/orgs/transfer-organization";
+import { createProject } from "./routes/projects/create-project";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -79,6 +80,8 @@ app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdonwOrganization)
 app.register(transferOrganization)
+
+app.register(createProject)
 
 app.listen({
   port: env.SERVER_PORT
